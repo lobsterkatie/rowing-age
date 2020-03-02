c4852c3e14e5738a0a34c0fff87bb05ed95722da
Katie Byers <lobsterkatie@gmail.com>
2020-03-02 13:41:06 -0800

    first pass at testing craft

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+- Add stuff here
+
+## 1.0.0
+
+Initial release
---
+++ scripts/bump-version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Example of a version-bumping script for an NPM project.
+### Located at: scripts/bump-version.sh
+
+# # helps with debugging
+# set -eux
+
+# # ${1} is argv[1]
+# OLD_VERSION="${1}"
+# NEW_VERSION="${2}"
+
+# # Do not tag and commit changes made by "npm version"
+# export npm_config_git_tag_version=false
+
+# # bump the version in package.json and package-lock.json
+# npm version "${NEW_VERSION}"
+
+echo "VERSION BUMP WOULD HAPPEN HERE"
+echo
+echo "CURRENT CONTENTS OF BUCKET"
+gsutil ls gs://craft-test-artifacts
+echo
