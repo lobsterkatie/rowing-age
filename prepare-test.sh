a461ff854161a086a3ae54d1beef4d6bbae3910b
Katie Byers <lobsterkatie@gmail.com>
2020-03-03 12:36:26 -0800

    use local craft and change build num every time

--- prepare-test.sh
+++ prepare-test.sh
@@ -11,6 +11,7 @@ source ignored/zeus-creds.sh
 # echo -e "\n$2 - zeus upload" >>testdoc.txt
 # echo $NOW >>testdoc.txt
 
-echo "git SHA of release branch head is $(git rev-parse release/1.0.1)"
-zeus job update --build 1 --job 1 --ref $(git rev-parse release/1.0.1)
-zeus upload --build 1 --job 1 testdoc.txt
+SHA=$(git rev-parse release/1.0.1)
+echo "git SHA of release branch head is $SHA"
+zeus job update --build $SHA --job 1 --ref $SHA
+zeus upload --build $SHA --job 1 testdoc.txt
--- publish-test.sh
+++ publish-test.sh
@@ -9,8 +9,9 @@ echo
 echo "RUNNING CRAFT PUBLISH"
 # craft publish --no-status-check $1
 # ./node_modules/.bin/craft publish --no-status-check --no-input 1.0.1 --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
+node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
 # npm run craft publish --no-status-check --no-input 1.0.1
-craft publish --no-status-check --no-input 1.0.1 --rev $(git rev-parse release/1.0.1)
+# craft publish --no-status-check --no-input 1.0.1 --rev $(git rev-parse release/1.0.1)
 
 echo
 echo "NEW CONTENTS OF BUCKET"
