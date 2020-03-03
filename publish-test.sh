e17192bc31ace977ba1a68ed12ab1fed8a81c962
Katie Byers <lobsterkatie@gmail.com>
2020-03-03 12:59:38 -0800

    make it merge the branch (we hope)

--- publish-test.sh
+++ publish-test.sh
@@ -9,7 +9,7 @@ echo
 echo "RUNNING CRAFT PUBLISH"
 # craft publish --no-status-check $1
 # ./node_modules/.bin/craft publish --no-status-check --no-input 1.0.1 --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
-node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
+node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 # --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
 # npm run craft publish --no-status-check --no-input 1.0.1
 # craft publish --no-status-check --no-input 1.0.1 --rev $(git rev-parse release/1.0.1)
 
