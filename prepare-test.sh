5e7b21ab4f6d041def92d0b151f422d2db5d47d1
Katie Byers <lobsterkatie@gmail.com>
2020-03-05 14:49:30 -0800

    move zeus stuff to version bump script

--- prepare-test.sh
+++ prepare-test.sh
@@ -6,13 +6,13 @@ export CRAFT_LOG_LEVEL=debug
 # craft prepare $1
 craft prepare 1.0.1
 
-echo "UPLOADING STUFF TO ZEUS"
-source ignored/zeus-creds.sh
-# NOW=$(date +"%r")
-# echo -e "\n$2 - zeus upload" >>testdoc.txt
-# echo $NOW >>testdoc.txt
+# echo "UPLOADING STUFF TO ZEUS"
+# source ignored/zeus-creds.sh
+# # NOW=$(date +"%r")
+# # echo -e "\n$2 - zeus upload" >>testdoc.txt
+# # echo $NOW >>testdoc.txt
 
-SHA=$(git rev-parse release/1.0.1)
-echo "git SHA of release branch head is $SHA"
-zeus job update --build $SHA --job 1 --ref $SHA
-zeus upload --build $SHA --job 1 testdoc.txt
+# SHA=$(git rev-parse release/1.0.1)
+# echo "git SHA of release branch head is $SHA"
+# zeus job update --build $SHA --job 1 --ref $SHA
+# zeus upload --build $SHA --job 1 testdoc.txt
