2aa66ee84f5ddf35af0e38b68e403b2d9661ded0
Katie Byers <lobsterkatie@gmail.com>
2020-03-05 17:56:57 -0800

    point to head again

--- zeus-make-build.sh
+++ zeus-make-build.sh
@@ -1,5 +1,5 @@
-SHA=$(git rev-parse release/1.0.1)
-# SHA=$(git rev-parse HEAD)
+# SHA=$(git rev-parse release/1.0.1)
+SHA=$(git rev-parse HEAD)
 # BUILD=$(git log --oneline | wc -l | sed -E 's/[ ]*(.*)/\1/')
 BUILD=$RANDOM
 echo "git SHA is $SHA"
