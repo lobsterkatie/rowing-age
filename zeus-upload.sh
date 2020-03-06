fc90598d6e491d04dde814984d6b111a91015206
Katie Byers <lobsterkatie@gmail.com>
2020-03-05 16:20:49 -0800

    use master head

--- zeus-upload.sh
+++ zeus-upload.sh
@@ -1,7 +1,8 @@
 echo "UPLOADING STUFF TO ZEUS"
 source ignored/zeus-creds.sh
 
-SHA=$(git rev-parse release/1.0.1)
+# SHA=$(git rev-parse release/1.0.1)
+SHA=$(git rev-parse HEAD)
 BUILD=$(git log --oneline | wc -l | sed -E 's/[ ]*(.*)/\1/')
 echo "git SHA is $SHA"
 echo "build number is $BUILD"
