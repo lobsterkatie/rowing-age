15899a92bde09ff0ee73aae4d337bbe7449b0b92
Katie Byers <lobsterkatie@gmail.com>
2020-03-06 07:15:17 -0800

    make output easier to read

--- full-flow-test.sh
+++ full-flow-test.sh
@@ -6,51 +6,46 @@
 source ignored/craft-env.sh
 # LOCAL_CRAFT="./node_modules/\@sentry/craft/dist/index.js"
 
-echo "RUNNING CRAFT PREPARE"
+echo -e "\nRUNNING CRAFT PREPARE"
 git branch -D release/1.0.1
 git push origin --delete release/1.0.1
 node ./node_modules/\@sentry/craft/dist/index.js prepare 1.0.1
 
-echo "MAKING FIRST ZEUS JOB"
+echo -e "\nMAKING FIRST ZEUS JOB"
 source ignored/zeus-creds.sh
 
 SHA=$(git rev-parse HEAD)
 BUILD=$RANDOM
-echo "git SHA is $SHA"
-echo "build number is $BUILD"
+echo -e "\ngit SHA is $SHA"
+echo -e "\nbuild number is $BUILD"
 zeus job update --build=$BUILD --job=1 --ref=$SHA
 
-echo "TAKING A BRIEF NAP"
+echo -e "\nTAKING A BRIEF NAP"
 sleep 30
 
-echo "MAKING SECOND ZEUS JOB"
+echo -e "\nMAKING SECOND ZEUS JOB"
 # SHA=$(git rev-parse HEAD)
 BUILD=$RANDOM
-echo "git SHA is $SHA"
-echo "build number is $BUILD"
+echo -e "\ngit SHA is $SHA"
+echo -e "\nbuild number is $BUILD"
 zeus job update --build=$BUILD --job=1 --ref=$SHA
 
-echo "UPLOADING STUFF TO ZEUS"
+echo -e "\nUPLOADING STUFF TO ZEUS"
 zeus upload --build=$BUILD --job=1 testdoc.txt
 
 source ignored/gcs-creds.sh
 unset ZEUS_TOKEN
 
-echo
-echo "EMPTYING BUCKET"
+echo -e "\nEMPTYING BUCKET"
 gsutil rm -r gs://craft-test-artifacts/1.0.1/*
-echo
-echo "CURRENT CONTENTS OF BUCKET"
+echo -e "\nCURRENT CONTENTS OF BUCKET"
 gsutil ls gs://craft-test-artifacts/1.0.1
-echo
 
-echo "RUNNING CRAFT PUBLISH"
+echo -e "\nRUNNING CRAFT PUBLISH"
 
 # SHA=$(git rev-parse HEAD)
 
 node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 --rev $SHA
 
-echo
-echo "NEW CONTENTS OF BUCKET"
+echo -e "\nNEW CONTENTS OF BUCKET"
 gsutil ls gs://craft-test-artifacts/1.0.1
-echo
