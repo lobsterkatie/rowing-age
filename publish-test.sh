d988ee2c57a0b04509b733ae5f3800699073dfb4
Katie Byers <lobsterkatie@gmail.com>
2020-03-05 17:21:59 -0800

    glimmers

--- publish-test.sh
+++ publish-test.sh
@@ -8,11 +8,14 @@ echo
 
 echo "RUNNING CRAFT PUBLISH"
 export CRAFT_LOG_LEVEL=debug
+SHA=$(git rev-parse release/1.0.1)
+
 # craft publish --no-status-check $1
 # ./node_modules/.bin/craft publish --no-status-check --no-input 1.0.1 --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
-node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 # --rev 38421686a7fdcee3cf984dd19a0dd13c2d05f98e
 # npm run craft publish --no-status-check --no-input 1.0.1
 # craft publish --no-status-check --no-input 1.0.1 --rev $(git rev-parse release/1.0.1)
+
+node ./node_modules/\@sentry/craft/dist/index.js publish --no-status-check --no-input 1.0.1 --rev $SHA
 
 echo
 echo "NEW CONTENTS OF BUCKET"
