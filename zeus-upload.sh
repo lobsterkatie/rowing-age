559dcde921b5841857dbf606c9e430542fa818d1
Katie Byers <lobsterkatie@gmail.com>
2020-03-05 17:31:34 -0800

    maybe

--- zeus-upload.sh
+++ zeus-upload.sh
@@ -1,8 +1,8 @@
 echo "UPLOADING STUFF TO ZEUS"
 source ignored/zeus-creds.sh
 
-SHA=$(git rev-parse release/1.0.1)
-# SHA=$(git rev-parse HEAD)
+# SHA=$(git rev-parse release/1.0.1)
+SHA=$(git rev-parse HEAD)
 # BUILD=$(git log --oneline | wc -l | sed -E 's/[ ]*(.*)/\1/')
 BUILD=$RANDOM
 echo "git SHA is $SHA"
